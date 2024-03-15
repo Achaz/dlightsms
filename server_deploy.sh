84d804f644af42d8cdeb89f3c16b4a14a81dec11
Aaron 410 <jtugume123@gmail.com>
2024-03-15 09:24:39 +0300

    Update server_deploy.sh

--- server_deploy.sh
+++ server_deploy.sh
@@ -4,13 +4,13 @@ set -e
 echo "Deploying application ..."
 
 # Enter maintenance mode
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+#(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
     # Update codebase
     git fetch origin main
-    git reset --hard origin/main
+    #git reset --hard origin/main
 
     # Install dependencies based on lock file
-    composer install --no-interaction --prefer-dist --optimize-autoloader
+    /usr/local/bin/composer install --no-interaction --prefer-dist --optimize-autoloader
 
     # Migrate database
     #php artisan migrate --force
@@ -19,7 +19,7 @@ echo "Deploying application ..."
     # ...
 
     # Clear cache
-    php artisan optimize
+    #php artisan optimize
 
     # Reload PHP to update opcache
     echo "" | sudo -S service php-fpm reload
