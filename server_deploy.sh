ff74c60f76309d8528bc99a3be76eb20a2163b6d
aaronburton410 <aaron.burton410@gmail.com>
2024-03-15 12:03:44 +0300

    changed something in the server_deploy script

--- server_deploy.sh
+++ server_deploy.sh
@@ -6,7 +6,7 @@ echo "Deploying application ..."
 # Enter maintenance mode
 #(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
     # Update codebase
-    git fetch origin main
+    git pull origin main
     #git reset --hard origin/main
 
     # Install dependencies based on lock file
